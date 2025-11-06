e5691c9db9e888b8d2769cbf6366c6c0989772bb
Franco <francodeniro.24@gmail.com>
2025-11-06 11:55:21 -0500

    InterfazTrascripcion

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./views/Login/Login";
 import LoginRegister from "./views/LoginRegister/LoginRegister";
 import Inicio from "./views/Inicio/Inicio";
-<<<<<<< HEAD
 import Transcripcion from "./views/Transcripcion/Transcripcion";
-=======
 import Ingesta from "./views/ingesta/Ingesta";
->>>>>>> origin/REACT
 
 
 
@@ -18,12 +15,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<LoginRegister />} />
         <Route path="/Inicio" element={<Inicio />} />
-<<<<<<< HEAD
         <Route path="/Transcripcion" element={<Transcripcion />} />
 
-=======
         <Route path="/ingesta" element={<Ingesta />} />
->>>>>>> origin/REACT
       </Routes>
     </BrowserRouter>
   );
--- Frontend/src/views/Inicio/Inicio.jsx
+++ Frontend/src/views/Inicio/Inicio.jsx
@@ -34,7 +34,8 @@ function Inicio() {
                                 <TbArrowDownFromArc className='text-[#196DFF] text-2xl' />
                                 <h3 className='font-bold text-xl sm:text-2xl text-[#196DFF]'>Ingesta</h3>
                                 <span className='text-xs sm:text-sm text-[#989898] pr-4'>Agregar nuevos videos a procesar</span>
-                            </Link>
+                            </article>
+
                             <article className='flex flex-col justify-center pl-4 gap-2 h-40 bg-white rounded-xl shadow-sm hover:shadow-md transition-shadow'>
                                 <HiDocumentText className='text-[#02B272] text-2xl' />
                                 <h3 className='font-bold text-xl sm:text-2xl text-[#02B272]'>Transcripción</h3>
